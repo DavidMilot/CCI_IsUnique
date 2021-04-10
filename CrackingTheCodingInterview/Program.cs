20ea7d2e5e094f5ababf73f222e748e9f5be91fd
DavidMilot <milot_david@hotmail.com>
2021-04-10 02:05:19 -0400

    1.1 complete

---
+++ CrackingTheCodingInterview/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CrackingTheCodingInterview
+{
+    class Program
+    {
+        bool IsUnique(string stringCheck)
+        {
+            Dictionary<char, int> dictionaryChar = new Dictionary<char, int>();
+
+            for (int i = 0; i < stringCheck.Length; i++)
+            {
+                if (dictionaryChar.ContainsKey(stringCheck[i]))
+                {
+                    Console.WriteLine(dictionaryChar.Count);
+                    return false;
+                }
+                else
+                {
+                    dictionaryChar.Add(stringCheck[i],i);
+                }
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            string sample = "Hello";
+
+            Console.WriteLine(p.IsUnique(sample).ToString());
+        }
+    }
+}
